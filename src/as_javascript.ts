3559cb4decbd89f267a38e2d860f7d2b9454ad54
ChiaMineJP <admin@chiamine.jp>
2021-06-14 22:28:56 +0900

    Might be ready to build

--- src/as_javascript.ts
+++ src/as_javascript.ts
@@ -1,22 +1,21 @@
 import {CastableType, NULL, SExp} from "./SExp";
 import {Bytes, t, Tuple2} from "./__type_compatibility__";
-import {CLVMObject} from "./CLVMObject";
 
-export type TOpStack = Function[];
-export type TValStack = Array<CLVMObject|Bytes|CLVMObject[]|Tuple2<CLVMObject, Bytes>>;
+export type TOpStack = Array<(op_stack: TOpStack, val_stack: TValStack) => unknown>;
+export type TValStack = Array<Bytes|SExp|SExp[]|Tuple2<SExp, SExp>>;
 export type TToSexpF = (arg: CastableType) => SExp;
 
 export function as_javascript(sexp: SExp){
   function _roll(op_stack: Function[], val_stack: TValStack){
-    const v1 = val_stack.pop() as CLVMObject|Bytes|Tuple2<CLVMObject, Bytes>;
-    const v2 = val_stack.pop() as CLVMObject|Bytes|Tuple2<CLVMObject, Bytes>;
+    const v1 = val_stack.pop() as SExp;
+    const v2 = val_stack.pop() as SExp;
     val_stack.push(v1);
     val_stack.push(v2);
   }
   
   function _make_tuple(op_stack: TOpStack, val_stack: TValStack){
-    const left = val_stack.pop() as CLVMObject;
-    const right = val_stack.pop() as Bytes;
+    const left = val_stack.pop() as SExp;
+    const right = val_stack.pop() as SExp;
     if(right.equal_to(NULL)){
       val_stack.push([left]);
     }
@@ -29,7 +28,7 @@ export function as_javascript(sexp: SExp){
     const left = [val_stack.pop()];
     const right = val_stack.pop();
     left.concat(right);
-    val_stack.push(left as CLVMObject[]);
+    val_stack.push(left as SExp[]);
   }
   
   function _as_javascript(op_stack: TOpStack, val_stack: TValStack){
@@ -45,7 +44,7 @@ export function as_javascript(sexp: SExp){
       }
       op_stack.push(_as_javascript);
       op_stack.push(_roll);
-      op_stack.push(as_javascript);
+      op_stack.push(_as_javascript);
       val_stack.push(left);
       val_stack.push(right);
     }
