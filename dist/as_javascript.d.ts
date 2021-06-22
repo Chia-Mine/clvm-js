52bdf29ce0eb7a8ba72819406ea6d6d1314e7e68
ChiaMineJP <admin@chiamine.jp>
2021-06-22 10:32:58 +0900

    Build

--- dist/as_javascript.d.ts
+++ dist/as_javascript.d.ts
@@ -3,4 +3,5 @@ import { Bytes, Tuple } from "./__type_compatibility__";
 export declare type TOpStack = Array<(op_stack: TOpStack, val_stack: TValStack) => unknown>;
 export declare type TValStack = Array<Bytes | SExp | SExp[] | Tuple<SExp, SExp>>;
 export declare type TToSexpF = (arg: CastableType) => SExp;
-export declare function as_javascript(sexp: SExp): SExp;
+export declare type TToJavascript = Bytes | Bytes[] | Tuple<TToJavascript, TToJavascript> | TToJavascript[];
+export declare function as_javascript(sexp: SExp): TToJavascript;
