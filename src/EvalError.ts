9a16e621ec8e49654f8a63678fde1f2c9b9913d5
ChiaMineJP <admin@chiamine.jp>
2021-06-26 00:55:52 +0900

    Made `EvalError::_sexp` public

--- src/EvalError.ts
+++ src/EvalError.ts
@@ -2,7 +2,7 @@ import type {str} from "./__python_types__";
 import type {CLVMObject} from "./CLVMObject";
 
 export class EvalError extends Error {
-  private _sexp?: CLVMObject;
+  public _sexp?: CLVMObject;
   name = "EvalError";
   
   public constructor(message: str, sexp: CLVMObject) {
