04b420d645b9fbab7a1050572d958be9e92938bf
ChiaMineJP <admin@chiamine.jp>
2021-06-28 01:01:12 +0900

    Fixed a minor issue

--- src/casts.ts
+++ src/casts.ts
@@ -1,5 +1,5 @@
 import {int, None} from "./__python_types__";
-import {Bytes} from "./__type_compatibility__";
+import {Bytes, h} from "./__type_compatibility__";
 
 // In javascript, max safe integer is 2**53-1 (53bit)
 // Surprisingly, parseInt() can parse over 32bit integer. 
@@ -7,7 +7,7 @@ export function int_from_bytes(b: Bytes|None): int {
   if(!b || b.length === 0){
     return 0;
   }
-  return parseInt(b.as_word().toString(), 16);
+  return parseInt(b.hex(), 16);
 }
 
 export function int_to_bytes(v: int): Bytes {
@@ -20,10 +20,10 @@ export function int_to_bytes(v: int): Bytes {
   else if(v > 0){
     let hexStr = v.toString(16);
     hexStr = hexStr.length % 2 ? "0" + hexStr : hexStr;
-    return Bytes.from(hexStr, "hex");
+    return h(hexStr);
   }
   else if(v >= -(2**32 - 1)){
-    return Bytes.from((v >>> 0).toString(16), "hex");
+    return h((v >>> 0).toString(16));
   }
   // if v is minus and over 32bit, split value into high and low bits.
   const highBits = (v / 2**32) >>> 0;
@@ -31,7 +31,7 @@ export function int_to_bytes(v: int): Bytes {
   const highHexStr = highBits.toString(16).length % 2 ? "0" + highBits.toString(16) : highBits.toString(16);
   const lowHexStr = lowBits.toString(16).length % 2 ? "0" + lowBits.toString(16) : lowBits.toString(16);
   const hexStr = highHexStr + lowHexStr;
-  return Bytes.from(hexStr, "hex");
+  return h(hexStr);
 }
 
 /**
