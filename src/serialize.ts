efae147eabf29d442546214c7f23de7a330a13a1
ChiaMineJP <admin@chiamine.jp>
2021-07-06 23:45:38 +0900

    Fixed a bug in `sexp_from_stream`

--- src/serialize.ts
+++ src/serialize.ts
@@ -17,6 +17,7 @@ import {Bytes, Stream, t} from "./__type_compatibility__";
 import {None} from "./__python_types__";
 import {TToSexpF, TValStack} from "./as_javascript";
 import {int_from_bytes, int_to_bytes} from "./casts";
+import {CLVMObject} from "./CLVMObject";
 
 const MAX_SINGLE_BYTE = 0x7F;
 const CONS_BOX_MARKER = 0xFF;
@@ -130,7 +131,7 @@ export function sexp_from_stream(f: Stream, to_sexp_f: TToSexpF){
   while(op_stack.length){
     const func = op_stack.pop();
     if(func){
-      func(op_stack, val_stack, f, (v: any) => new SExp(v));
+      func(op_stack, val_stack, f, ((v: any) => new CLVMObject(v)) as TToSexpF);
     }
   }
   
